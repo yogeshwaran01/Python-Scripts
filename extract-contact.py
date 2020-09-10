3bfa290858feb8839f436b19865b8dfb0564379d
YOGESHWARAN R <yogeshin247@gmail.com>
2020-09-10 17:09:29 +0530

    added extract-contact.py

---
+++ extract-contact.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""
+    Author: Yogeshwaran R
+    Description: Extract E-mail and Phone Number
+                 from website and textfiles
+    python extract-contact.py <filename>
+    python extract-contact.py <website> <0> or <1>
+    <0> =  website not get open
+    <1> =  website  get open
+    (Chrome driver must be install at current location")
+
+"""
+
+# Importing required Modules
+from selenium import webdriver
+import re
+import sys
+import bs4 as bs
+import requests
+import urllib.request
+import ssl
+
+#ssl certify
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#funtion to extract phone numbers
+def phone(text):
+    pattern = '(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'
+    result = re.findall(pattern,text)
+    return result
+
+#funtion to extract email-address
+def email(text):
+    pattern = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+    result = re.findall(pattern,text)
+    return result
+
+
+try:
+    url = sys.argv[1] #input for cmd arguement
+    if sys.argv[2] == "1":
+        try:
+            try: # for windows
+                driver = webdriver.Chrome(executable_path ="C:\Program Files (x86)\Google\Chrome\chromedriver.exe") 
+            except: # for wsl
+                driver = webdriver.Chrome(executable_path="/mnt/c/Program Files (x86)/Google/chrome/chromedriver.exe")
+            finally:    
+                driver.get(url)
+                html = driver.page_source
+                print("Extracted Phone Numbers from",url)
+                for i in phone(html):
+                    print(str(i).replace("'","").replace(" ","").replace(",",""))
+                print("Extracted E-mail address from",url)
+                for j in email(html):
+                    print(j)
+        except:
+            print("Acess Denied")
+        
+    elif sys.argv[2] == "0":
+        try:
+            source=urllib.request.urlopen(url).read()
+
+            soup=bs.BeautifulSoup(source,'html.parser')
+            print("Extracted Phone Numbers from",url)
+            for i in phone(soup.text):
+                print(str(i).replace("'","").replace(" ","").replace(",",""))
+            print("Extracted E-mail address from",url)
+            for j in email(soup.text):
+                print(j)
+        except urllib.error.HTTPError:
+            print("Acess Denied")
+        
+    else:
+        print("Unexcepted Arguements")
+        # if arguement is not url it is file
+except ValueError and IndexError:
+    file = open(sys.argv[1],"r")
+
+    for line in file:
+        for i in phone(line):
+            print(str(i).replace("'","").replace(" ","").replace(",",""))
+        for j in email(line):
+            print(j)    
