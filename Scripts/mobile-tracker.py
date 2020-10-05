1757a59a855f34489e92a41c8e8eb7ffa253a72d
YOGESHWARAN R <yogeshin247@gmail.com>
2020-10-05 17:42:32 +0530

    Update mobile-tracker.py

--- Scripts/mobile-tracker.py
+++ Scripts/mobile-tracker.py
@@ -1,34 +1,69 @@
 import sys
 
-import bs4 as bs
 import requests
+from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-
-url = "https://www.findandtrace.com/trace-mobile-number-location"
 
 headers = {"UserAgent": UserAgent().random}
 
+mobile_tracker_key = [
+    "Mobile Phone",
+    "Telecoms Circle / State",
+    "Network",
+    "Service Type / Signal",
+    "Connection Status",
+    "+91 9787814226 - SIM card distributed at",
+    "Owner / Name of the caller",
+    "Address / Current GPS Location",
+    "Number of Search History",
+    "Latest Search Places",
+    "Websites / social media contains this number",
+    "Other Telecoms operators in phone area",
+    "No.of district / region in the state",
+    "Circle Capital",
+    "Main Language in the telecoms circle",
+    "Other Languages in the telecom circle",
+    "Local time at phone location",
+    "How Lucky this Number",
+]
 
-def track_phone_number(number) -> list:
-    """
-    Funtion that track the mobile number
 
-    """
-    data = {"mobilenumber": number, "submit": number}
-    html = requests.post(url, data=data, headers=headers)
-    soup = bs.BeautifulSoup(html.text, "html.parser")
-    data_table = [i.text for i in soup.find_all("td")]
-    table_head = [i.text for i in soup.find_all("th")]
-    final = list(zip(table_head, data_table))
-    final.pop(12)
-    final.pop(-2)
-    return final
+class Track_Mobile_Number:
+    def __init__(self, indian_mobile_number):
+        self.url = "https://www.findandtrace.com/trace-mobile-number-location"
+        self.mobile_number = indian_mobile_number
+        if self.verify_number:
+            self.data = {
+                "mobilenumber": self.mobile_number,
+                "submit": self.mobile_number,
+            }
+        else:
+            raise Exception("Invalid Mobile Number")
+
+    @property
+    def verify_number(self):
+        if len(self.mobile_number) == 10 and self.mobile_number.isdigit():
+            return True
+        else:
+            return False
+
+    @property
+    def track(self) -> dict:
+        html = requests.post(self.url, data=self.data, headers=headers)
+        soup = BeautifulSoup(html.text, "html.parser")
+        if soup.find("title").text.strip() != "404 NOT FOUND":
+            mobile_tracker_valve = [i.text.strip() for i in soup.find_all("td")]
+            mobile_tracker = dict(zip(mobile_tracker_key, mobile_tracker_valve))
+            return mobile_tracker
+        else:
+            raise Exception("Mobile Number Not Found")
 
 
 if __name__ == "__main__":
     try:
-        mobile_number = sys.argv[1]
+        number = sys.argv[1]
     except (IndexError, KeyError):
-        mobile_number = str(input("Enter the Mobile Number: "))
-    for i in track_phone_number(mobile_number):
-        print(f"{i[0]} : {i[1]} \n")
+        number = str(input("Enter the Mobile number to track: "))
+    X = Track_Mobile_Number(number).track
+    for i in X:
+        print(f"{i} --> {X[i]}")
