e0dbcc279eef9bcd9b397ab95f711592755651a8
YOGESHWARAN R <yogeshin2547@gmail.com>
2020-10-16 17:43:07 +0530

    bug fix in auto readme.md

--- auto-generate-README.py
+++ auto-generate-README.py
@@ -1,6 +1,6 @@
 import os
 
-os.chdir(os.getcwd() + '/Scripts')
+os.chdir(os.getcwd() + "/Scripts")
 
 main = "https://github.com/yogeshwaran01/Python-Scripts/blob/master/Scripts/"
 
@@ -10,9 +10,9 @@ for i in os.listdir():
     s = f"| [{i}]({main + i}) |" + "\n"
     l.append(s)
 
-file = open("README.md",'w')
+file = open("README.md", "w")
 file.write("# Python-Scripts" + "\n" + "\n")
 file.write("Some python scripts" + "\n" + "\n")
-file.write("| **Scripts**|" + "\n")
-file.write("-------------- " + "\n")
+file.write(f"| **Scripts - {len(l)}**|" + "\n")
+file.write("|--------------|" + "\n")
 file.writelines(sorted(l))
